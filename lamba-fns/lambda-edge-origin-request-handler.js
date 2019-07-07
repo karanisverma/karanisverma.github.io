95dfcbb899b0952e0e7f169299d04c698ec007bf
karan verma <karan.verma@zoomcar.com>
2019-07-07 12:37:11 +0530

    fix: typo fixes and code formatting

--- lamba-fns/lambda-edge-origin-request-handler.js
+++ lamba-fns/lambda-edge-origin-request-handler.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Funciton registered on 'Origin Request' CloudFront Event
@@ -6,26 +6,21 @@
 exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
   const headers = request.headers;
-  const isBr =
-    headers["accept-encoding"] &&
-    headers["accept-encoding"][0].value.includes("br");
-  const isGzip =
-    headers["accept-encoding"] &&
-    headers["accept-encoding"][0].value.includes("gzip");
-  const gzipPath = "/gzip";
-  const brPath = "/br";
+  const isBr = headers['accept-encoding'] && headers['accept-encoding'][0].value.includes("br");
+  const isGzip = headers['accept-encoding'] && headers['accept-encoding'][0].value.includes("gzip");
+  const gzipPath = '/gzip';
+  const brPath = '/br';
 
   /**
-   * Update request path based on custom header
-   */
-
-  if (isBr) {
-    request.uri = brPath + request.url;
-  } else if (isGzip) {
-    request.uri = gzipPath + request.uri;
+  * Update request path based on custom header
+  */ 
+  if(isBr) {
+    request.uri = brPath + request.uri
+  } else if(isGzip) {
+    request.uri = gzipPath + request.uri
   }
-  console.log("isBR, isGzip-->", isBr, isGzip);
-  console.log("request.url -->", request.uri);
-  console.log("headers string-->", JSON.stringify(headers));
+  console.log('isBR, isGzip-->', isBr, isGzip)
+  console.log('request.url -->', request.uri);
+  console.log('headers string-->', JSON.stringify(headers))
   callback(null, request);
 };
